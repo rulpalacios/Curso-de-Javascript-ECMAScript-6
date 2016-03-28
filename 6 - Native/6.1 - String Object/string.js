469fe34a31fabc050a412670b13a45c838999ee4
Raul Palacios <palacios.raul02@gmail.com>
2016-03-28 10:52:08 -0600

    String Object

---
+++ 6 - Native/6.1 - String Object/string.js
@@ -0,0 +1,11 @@
+
+var cadena = "abc";
+var curso = "Curso de Javascript 2015";
+
+console.log(cadena.repeat(5));
+
+console.log(curso.includes("Javascript"));
+console.log(curso.includes("de",7));
+
+console.log(curso.startsWith("Javascript"));
+console.log(curso.endsWith("2015"));
