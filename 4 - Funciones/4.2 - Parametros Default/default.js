83dae202fe00889f483ff776f532d350131be122
Raul Palacios <palacios.raul02@gmail.com>
2016-03-28 10:16:25 -0600

    Funciones con Parametros por default

---
+++ 4 - Funciones/4.2 - Parametros Default/default.js
@@ -0,0 +1,9 @@
+/*
+ *	Parametros por Default
+ */
+
+function person(name = "Sin nombre", age = "Sin edad") {
+	console.log(`Hola mi nombre es ${name} y tengo ${age} años`);
+}
+
+person("Raul", 21);
