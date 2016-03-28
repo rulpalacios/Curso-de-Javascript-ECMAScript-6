cab06602e31e6bdd2d824c6ecd69be9e6c52a55e
Raul Palacios <palacios.raul02@gmail.com>
2016-03-28 10:55:55 -0600

    Number-Object

---
+++ 6 - Native/6.2 - Number Object/number.js
@@ -0,0 +1,11 @@
+
+
+console.log(Number.isNaN(12));
+console.log(Number.isNaN(0/0));
+
+console.log(Number.isFinite(Infinity));
+console.log(Number.isFinite(90));
+
+console.log(Number.isInteger(10));
+console.log(Number.isInteger("asd"));
+console.log(Number.isInteger(3.1416));
