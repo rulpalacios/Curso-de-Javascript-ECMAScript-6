b8c5262bbd4b632cc900b4e161abdd015564cec4
Raul Palacios <palacios.raul02@gmail.com>
2016-03-28 09:38:58 -0600

    Variables de Bloque

---
+++ 1 - Variables/1.2 - bloque/bloque.js
@@ -0,0 +1,14 @@
+/*
+*	Variables de bloque
+*/
+
+function suma(){
+	let resultado = 5 + 5;
+	console.log(`el resultado de la suma es ${resultado}`);
+}
+suma();
+
+for(let i = 0; i < 10; i++){
+	console.log(`El valor de i es ${i}`);
+}
+
