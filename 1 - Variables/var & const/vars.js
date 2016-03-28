14774f3b23ec0729e836da2dfbc033a5ea65b457
Raul Palacios <palacios.raul02@gmail.com>
2016-03-28 09:33:04 -0600

    Variables & Constantes

---
+++ 1 - Variables/var & const/vars.js
@@ -0,0 +1,14 @@
+/*
+*	Variables & Constantes
+*/
+
+var age = 21;
+var place = "Mexico";
+
+const NAME = "Raul";
+
+
+place = "España";
+age = 22;
+
+console.log(`Hola mi nombre es ${NAME} vivo en ${place} y tengo ${age} años`);
